a4963a191f709bdf9b0a053e9d5ee9afa99901f9
AlexGyver <alexgyvershow@ya.ru>
2025-01-05 12:50:43 +0300

    upd

--- webpack.index.js
+++ webpack.index.js
@@ -41,6 +41,19 @@ module.exports = {
                     "css-loader"
                 ]
             },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        targets: "defaults",
+                        presets: [
+                            ['@babel/preset-env']
+                        ]
+                    }
+                }
+            }
         ]
     },
 
